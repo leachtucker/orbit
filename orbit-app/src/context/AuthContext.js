7a9b68030dc08369f137f1199f00201ec135dc9a
Tucker <15221354+leachtucker@users.noreply.github.com>
2022-01-28 14:14:43 -0800

    Fetch initial state from localstorage

--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -1,16 +1,25 @@
 import React, { useState, createContext } from 'react';
-
 const AuthContext = createContext();
 const { Provider } = AuthContext;
 
 const AuthProvider = ({ children }) => {
-  const [authState, setAuthState] = useState({
-    token: null,
-    expiresAt: null,
-    userInfo: {}
-  });
+  const initialAuthState = () => {
+    const state = {
+      token: localStorage.getItem("token"),
+      expiresAt: localStorage.getItem("expiresAt"),
+      userInfo: JSON.parse(localStorage.getItem("userInfo") || {})
+    };
+
+    return state;
+  }
+
+  const [authState, setAuthState] = useState(initialAuthState);
 
   const setAuthInfo = ({ token, userInfo, expiresAt }) => {
+    localStorage.setItem("token", token);
+    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    localStorage.setItem("expiresAt", expiresAt);
+
     setAuthState({
       token,
       userInfo,
